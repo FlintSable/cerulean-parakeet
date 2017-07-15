cd467751f3e3b026f9dad9d166a2cf03459443bd
Bryan Castleman <castlemaninc@gmail.com>
2017-07-15 12:42:50 -0700

    adding new firebase connection file called Birdseed.js

---
+++ Birdseed.js
@@ -0,0 +1,11 @@
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAMnhNHY0kRxA5HmaX0RUumLfZMp3txXms",
+  authDomain: "birdseed-c2ffa.firebaseapp.com",
+  databaseURL: "https://birdseed-c2ffa.firebaseio.com",
+  storageBucket: "birdseed-c2ffa.appspot.com"
+};
+
+firebase.initializeApp(firebaseConfig);
